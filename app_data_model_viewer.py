3b902b78877f6f28ddeaaa00ec5235e557e73b4c
PaulOrasan <paul.orasan@gmail.com>
2024-06-06 18:30:37 +0300

    Add data viewer tab code

---
+++ app_data_model_viewer.py
@@ -0,0 +1,40 @@
+import gradio as gr
+
+from loaders.data_loader import fetch_available_datasets, fetch_dataset
+from loaders.experiment_loader import fetch_available_experiments, fetch_experiment_prediction
+
+datasets = fetch_available_datasets()
+experiments = fetch_available_experiments()
+
+
+def on_dataset_dropdown_change(event: gr.SelectData):
+    dataset = datasets[event.index]
+    return fetch_dataset(dataset.dataset_name, dataset.splits[0])
+
+
+def on_split_dropdown_change(data_index, event: gr.SelectData):
+    dataset = datasets[data_index]
+    return fetch_dataset(dataset.dataset_name, event.value)
+
+
+def on_dataset_change(data, event: gr.SelectData):
+    row = data.iloc[event.index[0]]
+    return row[0], row[1]
+
+
+def create_tab():
+    with gr.TabItem("Data Viewer", id=0) as tab:
+        with gr.Column():
+            with gr.Row():
+                dataset_dropdown = gr.Dropdown([d.dataset_name for d in datasets], type="index",
+                                               label="Select dataset:")
+                split_dropdown = gr.Dropdown(["train", "test"], label="Select split:")
+
+            dataframe = gr.DataFrame(headers=["Question", "Answer"], label="View data:")
+            question_box = gr.Textbox(label="Question:")
+            answer_box = gr.Textbox(label="Answer:")
+
+            dataset_dropdown.select(on_dataset_dropdown_change, inputs=[], outputs=[dataframe])
+            split_dropdown.select(on_split_dropdown_change, inputs=[dataset_dropdown], outputs=[dataframe])
+            dataframe.select(on_dataset_change, inputs=[dataframe], outputs=[question_box, answer_box])
+    return tab
